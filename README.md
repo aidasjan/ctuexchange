13a99f487556f818a6f04188d9a4d0faaaf6ba03
Aidas Jankauskas <aidas.jankauskas31@gmail.com>
2021-12-13 19:14:30 +0100

    overview and running instructions in readme, adding more data in init.sql

--- README.md
+++ README.md
@@ -1,11 +1,22 @@
 # University Exchange Application
- 
+
+## Overview
+Application is intended to make university exchange course selection more convenient by providing course recommendations from other universities and adding them to student's course list.
+It supports CRUD operations for students, universities, courses, tags, as well as many-to-many relationship for students-courses and courses-tags.
+More complex query is getting total amount of credits selected by students per university.
+
 ## Installation
 In order to run the application, the database needs to be set up.
 
 Steps to set up the database using Docker:
 1. Run `docker-compose up` to initialize the MySQL container.
-2. Connect to the database and execute `/sql/init.sql` script.
-3. Edit database credentials in `/src/main/resources/application.properties` if necessary.
+2. Connect to the database on port 3306 using crededntials provided in `/docker-compose.yml` and execute `/sql/init.sql` script.
 
+## Running the application
 
+### Server Application
+Server application can be started using `docker-compose up`. Port 8080 is used by server, port 3306 by database.
+
+### Client application
+Client application can be started by switching to the `/client` directory and running `npm start`. 
+It requires server to be running on `http://localhost:8080`.
--- sql/init.sql
+++ sql/init.sql
@@ -59,17 +59,38 @@ INSERT INTO universities (id, name, city) VALUES (1, 'Czech Technical University
 INSERT INTO universities (id, name, city) VALUES (2, 'Kaunas University of Technology', 'Kaunas');
 
 INSERT INTO students (id, name, surname, university_id) VALUES (1, 'John', 'Smith', 1);
-INSERT INTO students (id, name, surname, university_id) VALUES (2, 'William', 'Johnson', 1);
+INSERT INTO students (id, name, surname, university_id) VALUES (2, 'William', 'Johnson', 2);
 
 INSERT INTO courses (id, code, name, credits, university_id) VALUES (1, 'BIE-TJV', 'Java Technology', 5, 1);
-INSERT INTO courses (id, code, name, credits, university_id) VALUES (2, 'P000B000', 'Test Course', 6, 2);
+INSERT INTO courses (id, code, name, credits, university_id) VALUES (2, 'NIE-AM1', 'Middleware Architectures', 6, 1);
+INSERT INTO courses (id, code, name, credits, university_id) VALUES (3, 'MIE-IBE', 'Information Security', 6, 1);
+INSERT INTO courses (id, code, name, credits, university_id) VALUES (4, 'P000B001', 'Math', 6, 2);
+INSERT INTO courses (id, code, name, credits, university_id) VALUES (5, 'P000B002', 'Informatics', 6, 2);
+INSERT INTO courses (id, code, name, credits, university_id) VALUES (6, 'P000B003', 'Chemistry', 6, 2);
+INSERT INTO courses (id, code, name, credits, university_id) VALUES (7, 'P000B004', 'Software Engineering', 6, 2);
+INSERT INTO courses (id, code, name, credits, university_id) VALUES (8, 'P000B005', 'C#', 6, 2);
 
 INSERT INTO tags (id, name) VALUES (1, 'IT');
 INSERT INTO tags (id, name) VALUES (2, 'Chemistry');
+INSERT INTO tags (id, name) VALUES (3, 'Programming');
+INSERT INTO tags (id, name) VALUES (4, 'Math');
 
 INSERT INTO course_tags (id, tag_id, course_id) VALUES (1, 1, 1);
 INSERT INTO course_tags (id, tag_id, course_id) VALUES (2, 1, 2);
-INSERT INTO course_tags (id, tag_id, course_id) VALUES (3, 2, 2);
+INSERT INTO course_tags (id, tag_id, course_id) VALUES (3, 1, 3);
+INSERT INTO course_tags (id, tag_id, course_id) VALUES (4, 1, 5);
+INSERT INTO course_tags (id, tag_id, course_id) VALUES (5, 1, 7);
+INSERT INTO course_tags (id, tag_id, course_id) VALUES (6, 1, 8);
+INSERT INTO course_tags (id, tag_id, course_id) VALUES (7, 2, 6);
+INSERT INTO course_tags (id, tag_id, course_id) VALUES (8, 3, 1);
+INSERT INTO course_tags (id, tag_id, course_id) VALUES (9, 3, 2);
+INSERT INTO course_tags (id, tag_id, course_id) VALUES (10, 3, 8);
+INSERT INTO course_tags (id, tag_id, course_id) VALUES (11, 4, 4);
+INSERT INTO course_tags (id, tag_id, course_id) VALUES (12, 4, 5);
 
 INSERT INTO student_courses (id, student_id, course_id) VALUES (1, 1, 1);
-INSERT INTO student_courses (id, student_id, course_id) VALUES (2, 1, 2);
+INSERT INTO student_courses (id, student_id, course_id) VALUES (2, 1, 1);
+INSERT INTO student_courses (id, student_id, course_id) VALUES (3, 1, 1);
+INSERT INTO student_courses (id, student_id, course_id) VALUES (4, 1, 2);
+INSERT INTO student_courses (id, student_id, course_id) VALUES (5, 2, 7);
+INSERT INTO student_courses (id, student_id, course_id) VALUES (6, 2, 8);
